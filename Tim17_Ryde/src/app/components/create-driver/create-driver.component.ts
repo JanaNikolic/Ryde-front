a749ca6fba84bedcd5607ead3fe1a2e5d76f0c13
Bogdan <bogdanjanosevic@gmail.com>
2022-12-15 19:33:38 +0100

    Implemented Create  Driver Component

--- Tim17_Ryde/src/app/components/create-driver/create-driver.component.ts
+++ Tim17_Ryde/src/app/components/create-driver/create-driver.component.ts
@@ -9,6 +9,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class CreateDriverComponent {
 
   CreateDriverForm = new FormGroup({
+    FirstName: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    LastName: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    Email: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    Phone: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    Address: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    Licence: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    VehicleRegistration: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    Password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    ConfirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    Model: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    LicencePlate: new FormControl('', [Validators.required, Validators.minLength(1)]),
+    
+    
+
   })
+  ngOnInit(): void {}
+
+  create(){
+    if (!this.CreateDriverForm.valid){
+      alert("invalidForm")
+      
+
+    }
+
+  }
+  
+ 
 
 }
