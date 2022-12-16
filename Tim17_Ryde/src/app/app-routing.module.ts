59a941dda597f10723e4ce7f5c26263ad08c7268
Bogdan <bogdanjanosevic@gmail.com>
2022-12-16 16:31:02 +0100

    Creating admin main component

--- Tim17_Ryde/src/app/app-routing.module.ts
+++ Tim17_Ryde/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MainComponent } from './components/main/main.component';
 import { UnregisteredUserComponent } from './components/unregistered-user/unregistered-user.component';
+import { AdminMainComponent } from './components/admin-main/admin-main.component';
 
 const routes: Routes = [
   {path: '', component:MainComponent},
   {path: 'login', component:LoginComponent},
   {path: 'register', component:RegisterComponent},
   {path: 'get-started', component:UnregisteredUserComponent},
-  {path: 'home', component:MainComponent}
+  {path: 'home', component:MainComponent},
+  {path: 'admin-main', component:AdminMainComponent}
 ];
 
 @NgModule({
